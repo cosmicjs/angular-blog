d8f41d1bbffa868ffe590db878af78c617ecec38
codingherd <codingherd@gmail.com>
2018-09-26 17:31:15 +0530

    Minor changes

--- src/config/cosmo.config.ts
+++ src/config/cosmo.config.ts
@@ -3,7 +3,7 @@ export const config = {
     read_key: 'EhuqgAfS5PAkjF2UevYqh2BXJ2ICBZQGodveQwuKEez0RWR7CY',
     write_key: 'onfOne3PNQ2SH84U7gByZ7McXiRX0LWovzWVpSVWZXqit1lfGu',
     bucket_id: '5bab3c41f69d9e4f0a0408ba',
-    bucket_slug: 'angular-blog',
+    bucket_slug: 'angular-blog', 
     URL: 'https://api.cosmicjs.com/v1/',
     };
     
