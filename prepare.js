ad51326e621286884c6d4e37c1b67da7eac40a46
codingherd <codingherd@gmail.com>
2018-10-01 12:09:19 +0530

    minor changes

---
+++ prepare.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var str = `
+    export const config = {
+        production: true,
+        write_key: '${process.env.COSMIC_WRITE_KEY}',
+        bucket_name: '${process.env.COSMIC_BUCKET}',
+        
+    };
+`;
+fs.writeFile("./src/config/cosmic.prod.ts", str, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
---
+++ src/config/cosmic.prod.ts
@@ -0,0 +1,6 @@
+export const config = {
+    production: true,
+    write_key: 'onfOne3PNQ2SH84U7gByZ7McXiRX0LWovzWVpSVWZXqit1lfGu',
+    bucket_name: 'angular-blog',
+    
+};
