ba8d22ff46e3eea3236507fff61a2cf2fdd885b8
Tony Spiro <tspiro@tonyspiro.com>
2019-01-08 14:13:22 -0800

    add read key

--- prepare.js
+++ prepare.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var str = `
     export const config = {
         production: true,
+        read_key: '${process.env.COSMIC_READ_KEY}',
         write_key: '${process.env.COSMIC_WRITE_KEY}',
         bucket_name: '${process.env.COSMIC_BUCKET}',
         
